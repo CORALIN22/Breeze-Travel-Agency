253f4ca7d95208e843a142dab38e74fcb55fd3fe
shan-ti <thoselovelywords@gmail.com>
2024-08-22 18:17:46 +0900

    Done with the hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,15 @@
 export const Hero = () => {
   return (
     <>
-      <section className='bg-[url("../src/assets/imgs-brz/hero-min.jpg")]  bg-no-repeat bg-cover bg-center w-screen h-screen'>
-        <h1 className=''>Hello Breeze Travelers I came from the Hero Section</h1>
-        <button className="btnFilled">Try me </button>
-        <button className="btnOutlined">Try me </button>
+      <section className='bg-[url("../src/assets/imgs-brz/hero-min.jpg")] bg-no-repeat bg-cover bg-center w-screen h-[662px] px-[469px] py-[67px] flex-col justify-start items-center gap-10 inline-flex'>
+        <div className='flex-col justify-start items-center gap-2 flex'>
+          <h1>Elevate your journey, where tradition meets wanderlust</h1>
+          <h3>
+            Breeze Travel Agency believes every journey is a story waiting to be
+            told
+          </h3>
+        </div>
+        <button className='btnFilled'>Book Now</button>
       </section>
     </>
   );
