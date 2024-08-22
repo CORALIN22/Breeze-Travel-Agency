a3b6f090a0e9c31d754a332bbbbd54a8126e0e77
shan-ti <thoselovelywords@gmail.com>
2024-08-22 10:10:51 +0900

    modified all files and folders to vite

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,9 @@
+export const Hero = () => {
+  return (
+    <>
+      <section className='bg-[url("../src/assets/imgs-brz/hero-min.jpg")]  bg-no-repeat bg-cover bg-center w-screen h-screen'>
+        <h1 className='text-3xl text-brz-accent'>Hello Breeze Travelers I came from the Hero Section</h1>
+      </section>
+    </>
+  );
+}
