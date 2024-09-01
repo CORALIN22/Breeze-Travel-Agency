1784c1eec79ad4ce4952109cf10fcb89cdad5647
Coralin <coralinpableo22@gmail.com>
2024-09-01 14:58:15 +0900

    add new service section

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -12,40 +12,52 @@ export const Services=()=>{
                     <h1>Our Services</h1>
                 </div>
                 <div className='container flex justify-between items-center'>
-                    <div　className="flex gap-4 md:flex-col">
-                        <Link to="/" className="flex items-center border border-solid p-6 border-primary-500 rounded">
-                            <FontAwesomeIcon 
-                            icon={faBookOpen} // Use faCartShopping here
-                            style={{ color: "rgba(180, 136, 127, 1)" }} // Applying custom color
-                            className="text-xl w-10 h-10 border-0 border-inherit" // Tailwind class for icon size
-                            />
-                        </Link>
-                        <div className='flex items-center'>
-                            <p className='text-xl leading-7'>Curated <span className=''>Cultural</span> <br />Tours</p>
-                        </div>
-                    </div>
-                    <div　className="flex gap-4">
-                        <Link to="/" className="flex items-center border border-solid p-6 border-primary-500 rounded">
+                    <div　className="flex flex-col gap-4 justify-center items-center text-center w-1/3 lg:flex-row ">
+                        <Link to="/" className="w-24 h-24 border border-solid p-6 border-primary-500 rounded flex items-center justify-center">
                             <FontAwesomeIcon 
                             icon={faBookOpen} // Use faCartShopping here
                             style={{ color: "rgba(180, 136, 127, 1)" }} // Applying custom color
                             className="text-xl w-10 h-10" // Tailwind class for icon size
                             />
                         </Link>
-                        <div className='flex items-center'>
-                        <p className='text-xl leading-7'>Seasonal <br /><span>Experience and </span><br />Festivals</p>
+                        <div className='flex justify-center items-center text-left'>  
+                            <p className='text-xl leading-7 '>
+                                <span className='hidden lg:inline'>Curated </span>
+                                  Cultural
+                                <span className='block md:inline lg:block text-left'> Tours</span> 
+                            </p>
                         </div>
                     </div>
-                    <div　className="flex gap-4">
-                        <Link to="/" className="flex items-center border border-solid p-6 border-primary-500 rounded">
+                    <div　className="flex flex-col gap-4 justify-center items-center text-center w-1/3 lg:flex-row ">
+                        <Link to="/" className="w-24 h-24 border border-solid p-6 border-primary-500 rounded flex items-center justify-center">
                             <FontAwesomeIcon 
                             icon={faBookOpen} // Use faCartShopping here
                             style={{ color: "rgba(180, 136, 127, 1)" }} // Applying custom color
                             className="text-xl w-10 h-10" // Tailwind class for icon size
                             />
                         </Link>
-                        <div className='flex items-center'>
-                            <p className='text-xl leading-7'><span>Customized </span><br />Adventure<br />Packages</p>
+                        <div className='flex justify-center items-center text-left'>  
+                            <p className='text-xl leading-7 '>
+                                Seasonal
+                                <span className='hidden lg:block'>Experience and</span>
+                                <span className='block md:inline lg:block'> Festivals</span> 
+                            </p>
+                        </div>
+                    </div>
+                    <div　className="flex flex-col gap-4 justify-center items-center text-center w-1/3 lg:flex-row ">
+                        <Link to="/" className="w-24 h-24 border border-solid p-6 border-primary-500 rounded flex items-center justify-center">
+                            <FontAwesomeIcon 
+                            icon={faBookOpen} // Use faCartShopping here
+                            style={{ color: "rgba(180, 136, 127, 1)" }} // Applying custom color
+                            className="text-xl w-10 h-10" // Tailwind class for icon size
+                            />
+                        </Link>
+                        <div className='flex justify-center items-center text-left'>  
+                            <p className='text-xl leading-7 '>
+                                <span className='hidden lg:block'>Customized</span>
+                                Adventure
+                                <span className='block md:inline lg:block'> Packages</span> 
+                            </p>
                         </div>
                     </div>
                 </div>
